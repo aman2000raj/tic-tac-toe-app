99560acd1342703638b4bf837fcfc466f02b7dc3
aman2000raj <amankayshap10@gmail.com>
2024-05-02 02:08:25 +0530

    add component responsible for rendering game board

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Square from './Square'
+
+function Board() {
+  return (
+    <div className='board-row'>
+        <Square />
+    </div>
+  )
+}
+
+export default Board
