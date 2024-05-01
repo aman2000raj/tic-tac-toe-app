590e0af3edbc42141d48e6d4dacda2729840a602
aman2000raj <amankayshap10@gmail.com>
2024-05-02 02:09:34 +0530

    add component containing game logic.

---
+++ src/components/Game.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Board from './Board';
+
+function Game() {
+  return (
+    <div>
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
