2994c2f2678e4b08502f62455ac91ade596c1e08
aman2000raj <amankayshap10@gmail.com>
2024-05-02 13:57:57 +0530

    Add move functionality to tackmovement as game progress.

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,6 +12,25 @@ function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // Todo
+  } 
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+
   return (
     <div>
       <div className='game'>
@@ -23,7 +42,7 @@ function Game() {
           />
         </div>
         <div className='game-info'>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     </div>
