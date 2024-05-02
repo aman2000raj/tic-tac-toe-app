a7b0d0f60ef449ff482e4f725f758718e15498b9
aman2000raj <amankayshap10@gmail.com>
2024-05-02 13:34:37 +0530

    add history array to store list of arrays throughout the game and add handlePlay method to update the state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import Board from './Board';
 
 function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div>
       <div className='game'>
         <div className='game-board'>
-          <Board />
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
         </div>
         <div className='game-info'>
           <ol>{/* TODO */}</ol>
